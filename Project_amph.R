6731313e311df1d35d1233e3313657b9fc9b8d1b
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2023-05-31 12:58:07 +0200

    Update Project_amph.R

--- Project_amph.R
+++ Project_amph.R
@@ -15,92 +15,116 @@ library(readxl)
 #In questo dataset abbiamo 190 righe (osservazioni) e 23 colonne (variabili)
 #Inoltre abbiamo sia componenti biotiche (vegetazione e specie di anfibi) che abiotiche(numero di serbatoi, riserve idriche ecc...)
 
-Dataset_amph <- read_xlsx("data/amphibians.xlsx")
+
+dataset_amph <- read_xlsx("data/amphibians.xlsx")
 
 
 #Look at their structure
-view(Dataset_amph)
+view(dataset_amph)
 
-dim(Dataset_amph)
+dim(dataset_amph)
 
-head(Dataset_amph)
+head(dataset_amph)
 
-tail(Dataset_amph)
+tail(dataset_amph)
 
-str(Dataset_amph)
+str(dataset_amph)
 
-names(Dataset_amph)
+names(dataset_amph)
 
-
-glimpse(Dataset_amph)
+glimpse(dataset_amph)
 
 
 #2 DATA CLEANING----
 #Abbiamo elimianto le colonne non utili al nostro tipo di studio, in seguito abbiamo rinominato
 #delle colonne. 
 #Vedendo poi che la riga "ID" era uguale ai nomi delle varie colonne abbiamo deciso di eliminarla.
-New_Dataset_amph <- Dataset_amph[,c(-6, -7, -8, -9, -11, -12)]
-View(New_Dataset_amph)
+new_dataset_amph <- dataset_amph[,c(-6, -7, -8, -9, -11, -12)]
+view(new_dataset_amph)
 
+colnames(new_dataset_amph) <- c("ID",
+                                "motorway",
+                                "estensione_serb_mq",
+                                "mumero_serb/habitat",
+                                "tipo_serbatoio",
+                                "utilizzo_riserve_idriche",
+                                "dist_serb_strade",
+                                "dist_serb_edifici",
+                                "stato_serb",
+                                "tipolo_riva",
+                                "green_frogs",
+                                "brown_frogs",
+                                "common_toad",
+                                "fire-bellied_toad",
+                                "tree_frog",
+                                "common_newt",
+                                "great_crested_newt")
 
-colnames(New_Dataset_amph) <- c("ID",
-                                "Motorway",
-                                "Superficie_serbatoio_idrico_(m2)",
-                                "Numeri_di_serbatoi/habitat",
-                                "Tipo_di_Serbatoio",
-                                "Utilizzo_riserve_idriche",
-                                "Distanza_serbatoio-strade",
-                                "Distanza_serbatoio-edifici",
-                                "Stato_serbatoio",
-                                "Tipologia_riva",
-                                "Green_frogs",
-                                "Brown_frogs",
-                                "Common_toad",
-                                "Fire-bellied_toad",
-                                "Tree_frog",
-                                "Common_newt",
-                                "Great_crested_newt")
+new_dataset_amph <- new_dataset_amph[-1,]
 
-New_Dataset_amph <- New_Dataset_amph[-1,]
+head(new_dataset_amph)
 
-head(New_Dataset_amph)
-
-view(New_Dataset_amph)
-
-#Abbiamo convertito le variabili da dati qualitativi a quantitativi, in questo caso avevamo bisogno dei range in metri
-#delle strade 
-New_Dataset_amph["Distanza_serbatoio-strade"][New_Dataset_amph["Distanza_serbatoio-strade"] == 0] <- "<50 m"
-New_Dataset_amph["Distanza_serbatoio-strade"][New_Dataset_amph["Distanza_serbatoio-strade"] == 1] <- "50-100 m"
-New_Dataset_amph["Distanza_serbatoio-strade"][New_Dataset_amph["Distanza_serbatoio-strade"] == 2] <- "100-200 m"
-New_Dataset_amph["Distanza_serbatoio-strade"][New_Dataset_amph["Distanza_serbatoio-strade"] == 5] <- "200-500 m"
-New_Dataset_amph["Distanza_serbatoio-strade"][New_Dataset_amph["Distanza_serbatoio-strade"] == 9] <- "500-1000 m"
-New_Dataset_amph["Distanza_serbatoio-strade"][New_Dataset_amph["Distanza_serbatoio-strade"] == 10] <- ">1000 m"
-
-glimpse(New_Dataset_amph)
-view(New_Dataset_amph)
-
-
-#La colonna della superficie del serbatoio idrico ora non è più un character ma è stata modificata in numeric  
-New_Dataset_amph <- New_Dataset_amph %>%
-  mutate(
-    `Superficie_serbatoio_idrico_(m2)` = as.numeric(`Superficie_serbatoio_idrico_(m2)`)
-  )
-glimpse(New_Dataset_amph)
-
-
+view(new_dataset_amph)
 
 #3 DATA ANALYSIS----
-head(New_Dataset_amph)
-tail(New_Dataset_amph)
+head(new_dataset_amph)
+tail(new_dataset_amph)
+
+#La colonna della superficie del serbatoio idrico ora non è più un character ma è stata modificata in numeric  
+new_dataset_amph <- new_dataset_amph %>%
+  mutate(
+    estensione_serb_mq = as.numeric(estensione_serb_mq)
+  )
+glimpse(new_dataset_amph)
+
+
+
 
 #Ora vedremo qual è la specie con occorenza maggiore e quella con occorenza minore
-table(New_Dataset_amph$`Green frogs`)
-table(New_Dataset_amph$`Brown frogs`)
-table(New_Dataset_amph$`Common toad`)
-table(New_Dataset_amph$`Fire-bellied toad`)
-table(New_Dataset_amph$`Tree frog`)
-table(New_Dataset_amph$`Common newt`)
-table(New_Dataset_amph$`Great crested newt`)
+table(new_dataset_amph$`green_frogs`)
+table(new_dataset_amph$`brown_frogs`)
+table(new_dataset_amph$`common_toad`)
+table(new_dataset_amph$`fire-bellied_toad`)
+table(new_dataset_amph$`tree_frog`)
+table(new_dataset_amph$`common_newt`)
+table(new_dataset_amph$`great_crested_newt`)
 #La più frequente è "Brown Frog" e la meno frequente "Great crested newt" 
+
+# Creazione del database con tutte le osservazioni in cui Brown frogs sono presenti
+bfd <- new_dataset_amph %>%
+  filter(brown_frogs == '1')
+glimpse(bfd)
+unique(bfd$brown_frogs)
+unique(is.na(bfd$estensione_serb_mq))
+
+
+# Creazione del database con tutte le osservazioni in cui Great crested newt sono presenti
+gcnd <- new_dataset_amph %>%
+  filter(great_crested_newt == '1')
+glimpse(gcnd)
+unique(gcnd$great_crested_newt)
+
+
+#Durante lo studio verranno esaminati gli stessi parametri sia per la specie
+#"Brown frogs" che per quella "Great crested newt". Per praticità verranno
+#prima svolte tutte le analisi per la prima specie, e solo successivamente per la seconda.
+
+
+#statistiche di base di bfd
+bfd %>%
+  summarise(mean = mean(estensione_serb_mq),
+            sd = sd(estensione_serb_mq),
+            min = min(estensione_serb_mq),
+            max = max(estensione_serb_mq))
+
+
+gcnd %>%
+  summarise(mean = mean(estensione_serb_mq),
+            sd = sd(estensione_serb_mq),
+            min = min(estensione_serb_mq),
+            max = max(estensione_serb_mq))
+
+
+
 
 
